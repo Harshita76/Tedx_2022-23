259db3951739492ffda267b9e704859da5aee8ef
Akshat Khaitan <khaitanakshat40@gmail.com>
2021-07-31 17:06:54 +0530

    SideNavBar Added

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Sidebar.css";
+import { FiMic } from "react-icons/fi";
+import { RiContactsLine, RiGalleryLine } from "react-icons/ri";
+import { HiOutlineHome, HiOutlineUserGroup } from "react-icons/hi";
+import { Link, BrowserRouter } from "react-router-dom";
+
+function Sidebar() {
+	return (
+		<>
+			<BrowserRouter>
+				<div className="sidebar">
+					<div>
+						<Link to="#">
+							<HiOutlineHome className="icon" />
+						</Link>
+					</div>
+					<div>
+						<Link to="#">
+							<FiMic className="icon" />
+						</Link>
+					</div>
+					<div>
+						<Link to="#">
+							<HiOutlineUserGroup className="icon" />
+						</Link>
+					</div>
+					<div>
+						<Link to="#">
+							<RiGalleryLine className="icon" />
+						</Link>
+					</div>
+					<div>
+						<Link to="#">
+							<RiContactsLine className="icon" />
+						</Link>
+					</div>
+				</div>
+			</BrowserRouter>
+		</>
+	);
+}
+
+export default Sidebar;

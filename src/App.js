6e315df49c5eeb9f9d16a48d67d589a551436e91
nikhilmsharma <77634325+nikhilmsharma@users.noreply.github.com>
2021-08-05 18:10:50 +0530

    Added carousel(need to work on css)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Sidebar from './components/sidebar/Sidebar';
 import Homepage from './components/homepage/homepage_main.js';
+import Prevspk from './components/PreviousSpeakers/Carousel.babel.js'
+
 import {Switch, Router} from "react-router-dom";
 function App() {
   return (
     <>
+    
       <Sidebar />
       <Homepage />
+      
+      
+      <div class = "slideouter">
+      <Prevspk />
+      </div>
+      
+      
     </>
   );
 }
---
+++ src/components/PreviousSpeakers/Carousel.babel.js
@@ -0,0 +1,151 @@
+import React from 'react';
+import './Carousel.css';
+const slides = [
+    {
+      title: "Speaker-1",
+      subtitle: "Sub",
+      description: "descr",
+      image:"https://cdn.pixabay.com/photo/2016/11/09/16/24/virus-1812092__480.jpg"
+    },
+    {
+        title: "Speaker-2",
+        subtitle: "Sub",
+        description: "descr",
+        image:"https://cdn.pixabay.com/photo/2016/11/09/16/24/virus-1812092__480.jpg"
+    },
+    {
+        title: "Speaker-3",
+        subtitle: "Sub",
+        description: "descr",
+        
+        image:"https://cdn.pixabay.com/photo/2016/11/09/16/24/virus-1812092__480.jpg"
+    },
+    {
+        title: "Speaker-4",
+        subtitle: "Sub",
+        description: "descr",
+        image:"https://cdn.pixabay.com/photo/2016/11/09/16/24/virus-1812092__480.jpg"
+    },
+    {
+        title: "Speaker-5",
+        subtitle: "Sub",
+        description: "descr",
+        image:"https://cdn.pixabay.com/photo/2016/11/09/16/24/virus-1812092__480.jpg"
+    }
+  ];
+  
+  function useTilt(active) {
+    const ref = React.useRef(null);
+  
+    React.useEffect(() => {
+      if (!ref.current || !active) {
+        return;
+      }
+  
+      const state = {
+        rect: undefined,
+        mouseX: undefined,
+        mouseY: undefined
+      };
+  
+      let el = ref.current;
+  
+      const handleMouseMove = (e) => {
+        if (!el) {
+          return;
+        }
+        if (!state.rect) {
+          state.rect = el.getBoundingClientRect();
+        }
+        state.mouseX = e.clientX;
+        state.mouseY = e.clientY;
+        const px = (state.mouseX - state.rect.left) / state.rect.width;
+        const py = (state.mouseY - state.rect.top) / state.rect.height;
+  
+        el.style.setProperty("--px", px);
+        el.style.setProperty("--py", py);
+      };
+  
+      el.addEventListener("mousemove", handleMouseMove);
+  
+      return () => {
+        el.removeEventListener("mousemove", handleMouseMove);
+      };
+    }, [active]);
+  
+    return ref;
+  }
+  
+  const initialState = {
+    slideIndex: 0
+  };
+  
+  const slidesReducer = (state, event) => {
+    if (event.type === "NEXT") {
+      return {
+        ...state,
+        slideIndex: (state.slideIndex + 1) % slides.length
+      };
+    }
+    if (event.type === "PREV") {
+      return {
+        ...state,
+        slideIndex:
+          state.slideIndex === 0 ? slides.length - 1 : state.slideIndex - 1
+      };
+    }
+  };
+  
+  function Slide({ slide, offset }) {
+    const active = offset === 0 ? true : null;
+    const ref = useTilt(active);
+  
+    return (
+      <div
+        ref={ref}
+        className="slide"
+        data-active={active}
+        style={{
+          "--offset": offset,
+          "--dir": offset === 0 ? 0 : offset > 0 ? 1 : -1
+        }}
+      >
+        <div
+          className="slideBackground"
+          style={{
+            backgroundImage: `url('${slide.image}')`
+          }}
+        />
+        <div
+          className="slideContent"
+          style={{
+            backgroundImage: `url('${slide.image}')`
+          }}
+        >
+          <div className="slideContentInner">
+            <h2 className="slideTitle">{slide.title}</h2>
+            <h3 className="slideSubtitle">{slide.subtitle}</h3>
+            <p className="slideDescription">{slide.description}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  function Prevspk() {
+    const [state, dispatch] = React.useReducer(slidesReducer, initialState);
+  
+    return (
+      <div className="slides">
+        <button onClick={() => dispatch({ type: "PREV" })}>‹</button>
+  
+        {[...slides, ...slides, ...slides].map((slide, i) => {
+          let offset = slides.length + (state.slideIndex - i);
+          return <Slide slide={slide} offset={offset} key={i} />;
+        })}
+        <button onClick={() => dispatch({ type: "NEXT" })}>›</button>
+      </div>
+    );
+  }
+  
+  export default Prevspk;

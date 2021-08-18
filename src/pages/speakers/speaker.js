5d4bb3c0d2347d5cbea49f80f2e8d1e1f3ddf7ae
gowtham <gowthambandlapalli@gmail.com>
2021-08-18 21:08:01 +0530

    changed speaker_homepage

--- src/pages/speakers/speaker.js
+++ src/pages/speakers/speaker.js
@@ -1,18 +1,19 @@
 import React from "react";
-import mike from '../../assets/images/mictedx.png';
+import mike from '../../assets/images/Untitled design (9)/Untitled design (9)_1.png';
 import logo from '../../assets/images/TedXLogo.png';
 import './speaker.css';
+import video from '../../assets/images/latest_stage_video.mp4'
 
 const Speaker = () => {
     return(
         <>
             <div>
+                <video className="video" autoPlay loop muted>
+                    <source src={video} type='video/mp4'/>
+                </video>
+                <img src={logo} alt="logo" className='logo' />
+                <img src={mike} alt="mike" className='mike' />
                 <div className='speaker-homepage-background'>
-                    <img src={logo} className="tedx-logo" alt="" />
-                    <h1 className='speaker-homepage-title'>SPEAKERS</h1>
-                    <img className='speaker-homepage-img' src={mike} alt='' />
-                </div>
-                <div>
                 </div>
             </div>
         </>

3c9fbc946f7b43e56fda79d70ddbb270d627cf59
Akshat Khaitan <khaitanakshat40@gmail.com>
2021-09-02 10:56:07 +0530

    Form Media Queries Added

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -31,40 +31,46 @@ const Form = ({ submitForm }) => {
 						<h1 class="soon">Coming Soon</h1>
 					</div>
 				</div>
-				<div className="form-content-right">
+				<div className="form-right">
 					<form onSubmit={handleSubmit} className="form" noValidate>
 						<div class="wrapper2">
 							<div class="tedxicem">
-								<h2 class="ted">Contact Us.</h2>
+								<h2 class="ted">Contact Us</h2>
 							</div>
 						</div>
 						<div className="form-inputs">
-							<input
-								className="form-input"
-								type="text"
-								name="username"
-								placeholder="Name"
-								value={values.username}
-								onChange={handleChange}
-							/>
 							{errors.username && <p>{errors.username}</p>}
+							<div>
+								<input
+									className="form-input"
+									type="text"
+									name="username"
+									placeholder="Name"
+									value={values.username}
+									onChange={handleChange}
+								/>
+							</div>
 						</div>
 						<div className="form-inputs">
-							<input
-								className="form-input"
-								type="email"
-								name="email"
-								placeholder="Email"
-								value={values.email}
-								onChange={handleChange}
-							/>
 							{errors.email && <p>{errors.email}</p>}
+							<div>
+								<input
+									className="form-input"
+									type="email"
+									name="email"
+									placeholder="Email"
+									value={values.email}
+									onChange={handleChange}
+								/>
+							</div>
 						</div>
 						<div className="form-inputs">
-							<input
+							<textarea
 								className="form-input"
-								type="text"
+								type="textbox"
 								name="message"
+								cols="40"
+								rows="5"
 								placeholder="Message"
 								value={values.message}
 								onChange={handleChange}

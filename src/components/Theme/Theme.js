df6040750458e25c2fb4434d2b789ed2df800d8f
sushobhit1503 <65831876+sushobhit1503@users.noreply.github.com>
2021-10-04 11:55:59 +0530

    Update Theme.js

--- src/components/Theme/Theme.js
+++ src/components/Theme/Theme.js
@@ -21,7 +21,7 @@ export default function Theme() {
                     </Fade>
                     <Fade direction="right" delay={300} triggerOnce>
                         <div className="theme-text">
-                            <h5><span>TEDx</span> BPHC</h5>
+                            <h5><span>TEDx</span>BITSHyderabad</h5>
                             <p>Just twenty-six letters of the alphabet, combine in different ways to form words. Words that can change lives, words that can instigate an entire country to a revolution, or just the words that you are reading right now. Just seven notes of music come together and make up all of music. An entire art form, in all its glory and beauty, can be traced back to in seven sounds. Just two digits, zero and one, govern the entirety of computer science. Your laptops, iPads, phones (and probably your future robotic rulers, if the venerable Stephen Hawking is to be believed), all dictated by two numbers.</p>
                             <Link to="/contactus" type="button" className="theme_btn">Contact Us</Link>
                         </div>

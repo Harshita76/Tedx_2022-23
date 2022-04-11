a37ae8afef4261f366e0d24b84fc31de5939bf8d
Akshat Khaitan <khaitanakshat40@gmail.com>
2022-04-11 11:41:55 +0530

    Final Changes

--- src/components/other links/refund_policy.js
+++ src/components/other links/refund_policy.js
@@ -8,7 +8,7 @@ function RefundPolicy() {
       <div style={{display:"flex", flexDirection:"column", alignItems:"start", fontSize:"20px"}}>
 			<div>1. There is no refund or cancellation of the tickets allowed.</div>
 			<div>2. Tickets once sold cant be refunded.</div>
-			<div>3. The tickets are not transferable. </div>
+			<div>3. The tickets are not transferable.</div>
       </div>
 		</div>
 	);

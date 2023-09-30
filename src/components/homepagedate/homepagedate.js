51d6bed85fe27bafa6dfd08005836be3aa3d7b44
ads21022003 <79759920+ads21022003@users.noreply.github.com>
2023-09-30 21:33:42 +0530

    Update homepagedate.js

--- src/components/homepagedate/homepagedate.js
+++ src/components/homepagedate/homepagedate.js
@@ -1,1 +1,22 @@
+import React from "react";
+import "./homepagedate.css";
 
+export default function Homepagedate(){
+return(
+   <>
+<div class="main-wrapper">
+                <div class="date">
+                   <h1>28</h1>
+                   <h2>October</h2>
+                   <h2>2023</h2>
+                 </div>
+                <div class="location">
+                   <h2>AUDITORIUM</h2>
+                   <p>BITS PILANI HYDERABAD CAMPUS</p>
+                
+                </div>
+
+            </div>
+            </>
+);
+}

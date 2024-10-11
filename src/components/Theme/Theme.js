cadc903e5676093adba5a0c09d0dbea5ee8dcfb4
Vansh-Bh <vanshbhardwaj28134@gmail.com>
2024-10-11 19:17:23 +0530

    updated homepage theme

--- src/components/Theme/Theme.js
+++ src/components/Theme/Theme.js
@@ -4,14 +4,14 @@ import { Fade } from "react-awesome-reveal";
 import themeImage from "../../assets/images/theme.webp";
 // import { Link } from "react-router-dom";
 // import video1 from "../../assets/videos/theme.mp4";
-import THEME from "../../assets/images/THEME.png";
+import THEME from "../../assets/images/Theme_2024.png";
 
 export default function Theme() {
 	return (
 		<>
 			<section className="theme">
 				<Fade direction="up" delay={300} triggerOnce>
-					<h2 style={{textTransform:"uppercase"}}><span className="white">Theme - </span>For 2023 Talk Event </h2>
+					<h2 style={{textTransform:"uppercase"}}><span className="white">Ephemeral </span>Panorama </h2>
 				</Fade>
 				<div className="main">
 					<div className="theme-img">
@@ -24,7 +24,7 @@ export default function Theme() {
 								<span>Reflective </span> Tapestry
 							</h5> */}
 							<p>
-							In a world filled with diverse perspectives, experiences, our event aims to celebrate and unravel the intricate interplay of these threads that make up the rich fabric of our society. 'Reflective Tapestry' signifies the power of and introspection as we disentangle the stories woven into our lives, communities, and the world at large. Our speakers will delve into the threads of their own unique journeys, highlighting the beauty of connectivity, the importance of self-discovery, and the tapestry of human existence and experience. Join us in this extraordinary exploration of the myriad stories that shape our collective narrative, and be inspired to see the tapestry of the world through a new lens.
+							'Ephemeral Panorama' captures the fleeting essence of the vast and vivid human experience that we partake in throughout our lives. The objective of the theme is to display the wide landscape of the emotions, ideas, and episodes we face, which are momentary and yet inspire change to shape us into who we are. The lack of permanence in our thoughts, the swift change in knowledge, and a slower shift of wisdom show the brilliance of the transient nature of the lives we lead. In this altering panorama, we are often forced to re-examine our decisions and adapt for a newer goal with ever-shifting circumstances. The theme symbolizes the nature of impressions on our minds, and how they shape science, art, sports, and culture in incremental ways, urging us to see the big picture in the temporary moments of inspiration.
 							</p>
 							{/* <Link to="/contactus" type="button" className="theme_btn">
 								Contact Us

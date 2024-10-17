05848f0159a49517beb47a6329db1f036df5bc58
ATIKSH09 <atikshgupta6373@gmail.com>
2024-10-17 22:34:14 +0530

    new speakers added

--- src/components/newSpeaker/newData.js
+++ src/components/newSpeaker/newData.js
@@ -1,9 +1,14 @@
-import reveal from '../../assets/images/New_Speakers/RevealingSoon.png';
-import img1 from '../../assets/images/Speakers_2024/Archana_2024.jpg';
-import img2 from '../../assets/images/Speakers_2024/Unni_2024.jpg';
-import img3 from '../../assets/images/Speakers_2024/Aman_2024.jpg';
-import img4 from '../../assets/images/Speakers_2024/Vasundhara_2024.jpg';
-import img5 from '../../assets/images/Speakers_2024/Nitin_2024.jpg';
+import reveal from "../../assets/images/New_Speakers/RevealingSoon.png";
+import img1 from "../../assets/images/Speakers_2024/Archana_2024.jpg";
+import img2 from "../../assets/images/Speakers_2024/Unni_2024.jpg";
+import img3 from "../../assets/images/Speakers_2024/Aman_2024.jpg";
+import img4 from "../../assets/images/Speakers_2024/Vasundhara_2024.jpg";
+import img5 from "../../assets/images/Speakers_2024/Nitin_2024.jpg";
+import img6 from "../../assets/images/Speakers_2024/LT_gen_A_arjun.jpg";
+import img7 from "../../assets/images/Speakers_2024/Pallavi.jpg";
+import img8 from "../../assets/images/Speakers_2024/pavithra_lakshami.jpg";
+import img9 from "../../assets/images/Speakers_2024/mad_chef.jpg";
+
 export const slides = [
   {
     id: 1,
@@ -14,7 +19,7 @@ export const slides = [
     // link4: "https://twitter.com/zoyacaptain",
     // link5: "https://in.linkedin.com/in/captain-zoya",
     // description: "Captain Zoya Agarwal carries with her inspiring stories of following one's dreams irrespective of the barriers imposed on you. Having spent a childhood gazing up at the skies dreaming of becoming a pilot - Captain Zoya worked tirelessly to make her childhood dream her reality. Not to be stopped there, Captain Zoya continued breaking through glass ceilings. From becoming the youngest woman commander of the Boeing 777 to becoming the first woman commander of Air India to fly over the North Pole - Captain Zoya's path-breaking strides create pause for awe.",
-    image: img1
+    image: img1,
   },
   {
     id: 2,
@@ -25,7 +30,7 @@ export const slides = [
     // link4: "https://twitter.com/SeyfriedMonika",
     // link5: "https://www.linkedin.com/in/cs-unnikrishnan-b7893b20/",
     // description: "Monika Seyfried, an Interaction Designer who works across the disciplines of science, ethics and futures, has a strong background in art, photography and filmmaking. Monika engages at the intersection of emerging technologies, digital media, and the natural environment by creating sensory-rich, interactive spaces through her design work. Monika is currently a researcher at the Copenhagen Institute of Interaction Design , and one of her ongoing research initiatives is Grow Your Own Cloud, which began as a speculative exploration of DNA data storage methods.",
-    image: img2
+    image: img2,
   },
   {
     id: 3,
@@ -38,7 +43,7 @@ export const slides = [
     // link5: "https://in.linkedin.com/in/ranveerbrar",
 
     // description: "From a young boy on the streets of Lucknow to an international celebrity in the culinary world, Chef Ranveer’s story is one of dedication and perseverance. Founder of countless successful restaurants like Morisco, il Camino, Fishtail, in India as well as Banq in the US, Chef Ranveer is also the host of dozens of TV shows, and is a current judge of MasterChef India. His creativity, love for experimentation and eye for detail combined with his charming sense of humour make him one of the greatest Chefs of the country, and his videos never fail to bring a smile on his viewers’ faces.",
-    image: img3
+    image: img3,
   },
   {
     id: 4,
@@ -47,7 +52,7 @@ export const slides = [
     // link1: "https://open.spotify.com/artist/09UmIX92EUH9hAK4bxvHx6",
     // link2: "https://www.instagram.com/vasundharadas/",
     // description: " Devaagyh Dixit, India’s youngest and fastest drummer, made seven world records in a single day at the age of six. When Devaagyh started playing the drums at the age of two, his remarkably intuitive grasp of music and flair for learning that shone through was this wunderkind’s claim to fame. He made his first record at the age of 1 year and 10 months old, rightfully earning the title of ‘Grandmaster’ by the World Record University in London for his unmatched skill. Having performed over 160 drum solos by the mere age of six at both state and national level events, Devaagyh also plays an assortment of other instruments like the piano, guitar, conch, dombek, tabla and ghatam.",
-    image: img4
+    image: img4,
   },
   {
     id: 5,
@@ -59,6 +64,58 @@ export const slides = [
     // link5: "https://www.linkedin.com/in/nitin-a-gokhale-56b7b158/",
 
     // description: "Dr. Patrizia Azzi is an experimental particle physicist at the Istituto Nazionale di Fisica Nucleare (INFN), Italy. Her discoveries and contributions in unravelling the mysteries of the universe are countless. From her role in the Higgs discovery as the coordinator of the \"Physics and Performance Data\", to leading the studies for the determination of physics performance of an upgraded CMS detector and being a proponent of the project for a Future Circular Collider at CERN, Dr Azzi’s 20+ years of experience in the STEM field is aweinspiring and truly motivating for the young minds out there! Driven by her relentless passion, she has authored more than 1000 publications, with some prominent contribution with 500+ citations. We are pleased to have Dr Azzi share her ardent journey in elucidating the unknown.",
-    image: img5
+    image: img5,
+  },
+  {
+    id: 6,
+    title: "Lt Gen A Arun",
+    profession:
+      "Army retired after 40 years of service, on field strategy analyst ",
+    // link1: "https://www.instagram.com/neeta_lulla/",
+    // link3: "https://www.youtube.com/watch?v=-rr07Q3SrBA",
+    // link4: "https://twitter.com/patriziaazzi",
+    // link5: "https://www.linkedin.com/in/nitin-a-gokhale-56b7b158/",
+
+    // description: "Dr. Patrizia Azzi is an experimental particle physicist at the Istituto Nazionale di Fisica Nucleare (INFN), Italy. Her discoveries and contributions in unravelling the mysteries of the universe are countless. From her role in the Higgs discovery as the coordinator of the \"Physics and Performance Data\", to leading the studies for the determination of physics performance of an upgraded CMS detector and being a proponent of the project for a Future Circular Collider at CERN, Dr Azzi’s 20+ years of experience in the STEM field is aweinspiring and truly motivating for the young minds out there! Driven by her relentless passion, she has authored more than 1000 publications, with some prominent contribution with 500+ citations. We are pleased to have Dr Azzi share her ardent journey in elucidating the unknown.",
+    image: img6,
+  },
+  {
+    id: 7,
+    title: "⁠Pallavi Balhotra",
+    profession:
+      "POSH Awareness expert ",
+    // link1: "https://www.instagram.com/neeta_lulla/",
+    // link3: "https://www.youtube.com/watch?v=-rr07Q3SrBA",
+    // link4: "https://twitter.com/patriziaazzi",
+    // link5: "https://www.linkedin.com/in/nitin-a-gokhale-56b7b158/",
+
+    // description: "Dr. Patrizia Azzi is an experimental particle physicist at the Istituto Nazionale di Fisica Nucleare (INFN), Italy. Her discoveries and contributions in unravelling the mysteries of the universe are countless. From her role in the Higgs discovery as the coordinator of the \"Physics and Performance Data\", to leading the studies for the determination of physics performance of an upgraded CMS detector and being a proponent of the project for a Future Circular Collider at CERN, Dr Azzi’s 20+ years of experience in the STEM field is aweinspiring and truly motivating for the young minds out there! Driven by her relentless passion, she has authored more than 1000 publications, with some prominent contribution with 500+ citations. We are pleased to have Dr Azzi share her ardent journey in elucidating the unknown.",
+    image: img7,
+  },
+  {
+    id: 8,
+    title: "Pavithra Lakshmi",
+    profession:
+      "Versatile actress in Tamil and Malayalam industries",
+    // link1: "https://www.instagram.com/neeta_lulla/",
+    // link3: "https://www.youtube.com/watch?v=-rr07Q3SrBA",
+    // link4: "https://twitter.com/patriziaazzi",
+    // link5: "https://www.linkedin.com/in/nitin-a-gokhale-56b7b158/",
+
+    // description: "Dr. Patrizia Azzi is an experimental particle physicist at the Istituto Nazionale di Fisica Nucleare (INFN), Italy. Her discoveries and contributions in unravelling the mysteries of the universe are countless. From her role in the Higgs discovery as the coordinator of the \"Physics and Performance Data\", to leading the studies for the determination of physics performance of an upgraded CMS detector and being a proponent of the project for a Future Circular Collider at CERN, Dr Azzi’s 20+ years of experience in the STEM field is aweinspiring and truly motivating for the young minds out there! Driven by her relentless passion, she has authored more than 1000 publications, with some prominent contribution with 500+ citations. We are pleased to have Dr Azzi share her ardent journey in elucidating the unknown.",
+    image: img8,
+  },
+  {
+    id: 9,
+    title: "Chef Koushik Shankar",
+    profession:
+      "Renowned Chef,  Food and Beverage Retail professional",
+    // link1: "https://www.instagram.com/neeta_lulla/",
+    // link3: "https://www.youtube.com/watch?v=-rr07Q3SrBA",
+    // link4: "https://twitter.com/patriziaazzi",
+    // link5: "https://www.linkedin.com/in/nitin-a-gokhale-56b7b158/",
+
+    // description: "Dr. Patrizia Azzi is an experimental particle physicist at the Istituto Nazionale di Fisica Nucleare (INFN), Italy. Her discoveries and contributions in unravelling the mysteries of the universe are countless. From her role in the Higgs discovery as the coordinator of the \"Physics and Performance Data\", to leading the studies for the determination of physics performance of an upgraded CMS detector and being a proponent of the project for a Future Circular Collider at CERN, Dr Azzi’s 20+ years of experience in the STEM field is aweinspiring and truly motivating for the young minds out there! Driven by her relentless passion, she has authored more than 1000 publications, with some prominent contribution with 500+ citations. We are pleased to have Dr Azzi share her ardent journey in elucidating the unknown.",
+    image: img9,
   },
 ];
